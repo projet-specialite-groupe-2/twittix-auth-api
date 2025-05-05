2d4a959359d727d6f46706d014a190a8ad2e04c3
kevindecaux <decaux.kevin@hotmail.com>
2025-05-05 17:54:13 +0200

    [FIX] add bearer token in call to api and add somme control

--- app/services/user_manager.py
+++ app/services/user_manager.py
@@ -3,26 +3,43 @@ from app.models.user import BackendUser
 import requests
 
 
-def create_user(user):
+def create_user(user, token):
     """Crée un nouvel utilisateur via une API externe."""
-    response = requests.post(settings.BACKEND_URL + "/api/users", json=user)
+    headers = {"Authorization": f"Bearer {token}"}
+    try:
+        response = requests.post(
+            settings.BACKEND_URL + "/api/users", json=user, headers=headers
+        )
+    except requests.exceptions.RequestException as e:
+        return {
+            "error": f"Erreur de connexion à l'API : {str(e)}"
+        }, response.status_code
     if response.status_code == 201:
-        return response.json()
+        return response.json(), response.status_code
     else:
-        return {"error": "Erreur lors de la création de l'utilisateur"}
+        error = response.json().get("detail", "Erreur inconnue")
+        return (
+            {"error": f"Erreur lors de la création de l'utilisateur: {error}"},
+            response.status_code,
+        )
 
 
-def patch_user(user_data, email):
+def patch_user(user_data, email, token):
     """Met à jour les informations d'un utilisateur via une API externe."""
-
-    user = requests.get(settings.BACKEND_URL + "/api/users?email=" + email)
+    headers = {"Authorization": f"Bearer {token}"}
+    user = requests.get(
+        settings.BACKEND_URL + "/api/users?email=" + email, headers=headers
+    )
     user = user.json()
     user = BackendUser(**user[0])
 
     response = requests.patch(
         settings.BACKEND_URL + "/api/users/" + str(user.id),
         json=user_data,
-        headers={"Content-Type": "application/merge-patch+json"},
+        headers={
+            "Content-Type": "application/merge-patch+json",
+            "Authorization": f"Bearer {token}",
+        },
     )
     if response.status_code == 200:
         return response.json()
